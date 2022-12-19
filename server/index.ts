aaa2bf75b1a5bc356fb71c1ff8ad0f0da9da9355
jonatchoum <jona.atsu@gmail.com>
2022-12-19 15:36:32 +0100

    DB

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import mysql from "mysql2";
+
 const app = express();
 
 const PORT = 3000;
 app.get("/api", (req, res) => {
   res.json({ response: "API LOADED !" });
 });
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "FAKE_DB",
+  password: "root",
+});
+
+connection.query("SELECT * FROM `Users` WHERE 1", (err, results) => {
+  console.log(results);
+});
+
+connection.end();
 
 app.listen(PORT, () =>
   console.log(`App listening on port ${PORT}\nhttp://localhost:3000/api`)
