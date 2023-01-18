1e3d0cd4488f95357c71f5af50821ca35b41957c
jonatchoum <jona.atsu@gmail.com>
2023-01-18 22:38:48 +0100

    quick changes

--- client/src/Hooks/useUpdateUser.tsx
+++ client/src/Hooks/useUpdateUser.tsx
@@ -2,9 +2,22 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { toast } from "react-toastify";
 
+type UserInfo = {
+  id?: number;
+  username?: string;
+  hash_password?: string;
+  prenom?: string;
+  nom?: string;
+  role?: string;
+  email?: string;
+  societe?: string;
+  password?: string;
+  admin?: boolean;
+};
+
 const useUpdateUser = () => {
   return useMutation({
-    mutationFn: (user) => {
+    mutationFn: (user: UserInfo) => {
       return axios.patch("/updateuser", user);
     },
     onError: () => {
--- client/src/Pages/UpdateUser.tsx
+++ client/src/Pages/UpdateUser.tsx
@@ -1,5 +1,5 @@
 import { Button, Checkbox, Input, Loader } from "@mantine/core";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import queryUser from "../Hooks/queryUser";
@@ -7,10 +7,6 @@ import useUpdateUser from "../Hooks/useUpdateUser";
 
 const UpdateUser = () => {
   const { id } = useParams();
-  // const [userInfoToChange, setUserInfoToChange] = useState<object>({
-  //   id: id,
-  //   admin: "false",
-  // });
 
   const user = queryUser(id);
   const mutation = useUpdateUser();
@@ -22,10 +18,9 @@ const UpdateUser = () => {
     return <>Error</>;
   }
 
-  // const handleSubmit = (e: React.SyntheticEvent) => {
-  //   e.preventDefault();
-  //   mutation.mutate(user);
-  // };
+  const onSubmit = (data: any) => {
+    mutation.mutate(data);
+  };
 
   const {
     handleSubmit,
@@ -33,109 +28,62 @@ const UpdateUser = () => {
     register,
   } = useForm();
 
-  console.log(userInfoToChange);
   return (
     <div className="grid gap-1 mb-10">
       <h1>
         Modifier {user.data.prenom} {user.data.nom}
       </h1>
-      <form className="grid gap-2 " onSubmit={handleSubmit}>
+      <form className="grid gap-2 " onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="username">username</label>
           <Input
-            name="username"
             type="text"
             placeholder={user.data.username}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                username: e.target.value,
-              })
-            }
+            {...register("username")}
           />
         </div>
         <div>
           <label htmlFor="prenom">prenom</label>
           <Input
-            name="prenom"
             type="text"
             placeholder={user.data.prenom}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                prenom: e.target.value,
-              })
-            }
+            {...register("prenom")}
           />
         </div>
         <div>
           <label htmlFor="nom">nom</label>
-          <Input
-            name="nom"
-            type="text"
-            placeholder={user.data.nom}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                nom: e.target.value,
-              })
-            }
-          />
+          <Input type="text" placeholder={user.data.nom} {...register("nom")} />
         </div>
         <div>
           <label htmlFor="role">role</label>
           <Input
-            name="role"
             type="text"
             placeholder={user.data.role}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                role: e.target.value,
-              })
-            }
+            {...register("role")}
           />
         </div>
         <div>
           <label htmlFor="email">email</label>
           <Input
-            name="email"
             type="text"
             placeholder={user.data.email}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                email: e.target.value,
-              })
-            }
+            {...register("email")}
           />
         </div>
         <div>
           <label htmlFor="societe">societe</label>
           <Input
-            name="societe"
             type="text"
             placeholder={user.data.societe}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                societe: e.target.value,
-              })
-            }
+            {...register("societe")}
           />
         </div>
         <div>
           <label htmlFor="admin">admin</label>
           <Checkbox
-            name="admin"
             size="md"
             defaultChecked={user.data.admin}
-            onChange={(e) =>
-              setUserInfoToChange({
-                ...userInfoToChange,
-                admin: e.target.checked,
-              })
-            }
+            {...register("admin")}
           ></Checkbox>
         </div>
         <Button type="submit">modifier</Button>
