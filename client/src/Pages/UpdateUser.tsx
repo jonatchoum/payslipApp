8e7d9e514c8922e5ab0a5ef6ac201604233d66da
jonatchoum <jona.atsu@gmail.com>
2023-01-19 10:54:52 +0100

    update user form validation

--- client/src/Pages/UpdateUser.tsx
+++ client/src/Pages/UpdateUser.tsx
@@ -4,12 +4,35 @@ import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import queryUser from "../Hooks/queryUser";
 import useUpdateUser from "../Hooks/useUpdateUser";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const UserInfoSchema = z.object({
+  username: z.string().optional(),
+  prenom: z.string().optional(),
+  nom: z.string().optional(),
+  role: z.string().optional(),
+  email: z.string().email().optional().or(z.literal("")),
+  societe: z.string().optional(),
+  admin: z.boolean().optional(),
+});
 
 const UpdateUser = () => {
   const { id } = useParams();
-
   const user = queryUser(id);
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+  } = useForm({ resolver: zodResolver(UserInfoSchema) });
+
   const mutation = useUpdateUser();
+
+  const onSubmit = (data: any) => {
+    console.log(errors);
+    // console.log({ id, ...data });
+    mutation.mutate({ id, ...data });
+  };
 
   if (user.isLoading) {
     return <Loader />;
@@ -17,16 +40,6 @@ const UpdateUser = () => {
   if (user.isError) {
     return <>Error</>;
   }
-
-  const onSubmit = (data: any) => {
-    mutation.mutate(data);
-  };
-
-  const {
-    handleSubmit,
-    formState: { errors },
-    register,
-  } = useForm();
 
   return (
     <div className="grid gap-1 mb-10">
@@ -41,6 +54,9 @@ const UpdateUser = () => {
             placeholder={user.data.username}
             {...register("username")}
           />
+          {errors?.username?.message && (
+            <p className="text-red-500 text-xs">{errors.username?.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="prenom">prenom</label>
@@ -49,10 +65,16 @@ const UpdateUser = () => {
             placeholder={user.data.prenom}
             {...register("prenom")}
           />
+          {errors?.prenom?.message && (
+            <p className="text-red-500 text-xs">{errors.prenom?.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="nom">nom</label>
           <Input type="text" placeholder={user.data.nom} {...register("nom")} />
+          {errors?.nom?.message && (
+            <p className="text-red-500 text-xs">{errors.nom?.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="role">role</label>
@@ -61,6 +83,9 @@ const UpdateUser = () => {
             placeholder={user.data.role}
             {...register("role")}
           />
+          {errors?.role?.message && (
+            <p className="text-red-500 text-xs">{errors.role?.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="email">email</label>
@@ -69,6 +94,9 @@ const UpdateUser = () => {
             placeholder={user.data.email}
             {...register("email")}
           />
+          {errors?.email?.message && (
+            <p className="text-red-500 text-xs">{errors.email?.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="societe">societe</label>
@@ -77,6 +105,9 @@ const UpdateUser = () => {
             placeholder={user.data.societe}
             {...register("societe")}
           />
+          {errors?.societe?.message && (
+            <p className="text-red-500 text-xs">{errors.societe?.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="admin">admin</label>
@@ -85,10 +116,13 @@ const UpdateUser = () => {
             defaultChecked={user.data.admin}
             {...register("admin")}
           ></Checkbox>
+          {errors?.admin?.message && (
+            <p className="text-red-500 text-xs">{errors.admin?.message}</p>
+          )}
         </div>
         <Button type="submit">modifier</Button>
       </form>
-      <Button className="bg-red-500">annuler</Button>
+      <Button className="bg-red-500 text-xs">annuler</Button>
     </div>
   );
 };
