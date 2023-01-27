ffab595d81af4fcaeeb20a4fd0b2ce702ea1f8fd
jon <jona.atsu@gmail.com>
2023-01-27 12:20:10 +0100

    dotenv variables added

--- server/src/db/dbConn.ts
+++ server/src/db/dbConn.ts
@@ -1,8 +1,8 @@
-import mysql from "mysql2/promise";
+// import mysql from "mysql2/promise";
 
-export const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "FAKE_DB",
-  password: "root",
-});
+// export const connection = await mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "FAKE_DB",
+//   password: "root",
+// });
