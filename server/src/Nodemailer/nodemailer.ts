114da49cdbbe718831fe257971e23ed75f1c5f83
jon <jona.atsu@gmail.com>
2023-02-13 10:21:30 +0100

    nodemailer dotenv config

--- server/src/Nodemailer/nodemailer.ts
+++ server/src/Nodemailer/nodemailer.ts
@@ -1,20 +1,20 @@
 import nodemailer from "nodemailer";
 
-// let transporter = nodemailer.createTransport({
-//     host: "smtp.ethereal.email",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: testAccount.user, // generated ethereal user
-//       pass: testAccount.pass, // generated ethereal password
-//     },
-//   });
-
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: parseInt(process.env.MAIL_PORT as string),
-  secure: false, // upgrade later with STARTTLS
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAIL_HOST,
+//   port: parseInt(process.env.MAIL_PORT as string),
+//   secure: false, // upgrade later with STARTTLS
+// });
 
 const testNodemailer = () => {
   transporter.verify(function (error) {
