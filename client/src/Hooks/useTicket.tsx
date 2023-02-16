ca46b2553cd6ca913859aae7495af39f391d464d
jon <jona.atsu@gmail.com>
2023-02-16 08:59:25 +0100

    useTicket hook

---
+++ client/src/Hooks/useTicket.tsx
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const postTicket = (data: { id: string; sujet: string; details: string }) => {
+  return axios.post("/ticket", data);
+};
+
+const useTicket = () => {
+  return useMutation({
+    mutationFn: postTicket,
+    onSuccess: () => {
+      toast.success("Ticket envoyé");
+    },
+    onError: () => {
+      toast.error("Ticket non envoyé");
+    },
+  });
+};
+
+export { useTicket };
